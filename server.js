63a1bc61ceff5d797fae1edf37a97b47010a97b6
pcion <patrick.ciongoli@gmail.com>
2022-03-20 15:36:37 -0400

    successfulyl connected to server

--- server.js
+++ server.js
@@ -1,4 +1,38 @@
 const express = require('express')
+const fs = require('fs')
 
-const PORT = porocess.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('index.html');
+})
+
+app.get('/notes', (req, res) => {
+    res.send('notes.html');
+})
+
+
+app.get('/api/notes', (req, res) => {
+    var db = fs.readFile('db/db.json', 'utf-8', (err, data) => {
+        if (err) { console.log(err); return; }
+        console.log("Successfully read json")
+        console.log(db);
+        res.send(data);
+    });
+    
+    
+})
+
+// test endpoint
+app.post('/test', (req, res) => {
+    var temp = req.body['note_name'];
+    res.send("Hi this is a note" + temp);
+})
+
+app.listen(PORT, () => {
+    console.log('Server connected at:', PORT);
+});
